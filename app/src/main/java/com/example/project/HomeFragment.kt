2617539d708f20009d82d885c5880f612b988d2c
qjmsantos01 <joshuasantos80@yahoo.com>
2021-11-11 10:49:26 +0000

    Commit version1

--- app/src/main/java/com/example/project/HomeFragment.kt
+++ app/src/main/java/com/example/project/HomeFragment.kt
@@ -1,12 +1,11 @@
 package com.example.project
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.project.databinding.FragmentHomeBinding
 
 
@@ -30,8 +29,19 @@ class HomeFragment : Fragment() {
         binding.btnmonitor.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_homeFragment2_to_monitorFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
 
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean
+    {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 }
